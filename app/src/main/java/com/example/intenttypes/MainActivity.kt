bf47f6c5c401cbeb17cf47e0f179eafa184fa2cb
nabeelah030 <80572116+nabeelah030@users.noreply.github.com>
2021-12-14 20:55:49 +0500

    email_intents_030

--- app/src/main/java/com/example/intenttypes/MainActivity.kt
+++ app/src/main/java/com/example/intenttypes/MainActivity.kt
@@ -7,5 +7,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var eml =findViewById<Button>(R.id.email)
+
+         eml.setOnClickListener {
+            sendemail()
+        }
+    }
+     private fun sendemail() {
+        var emil = arrayOf("Nabeel2525.na@gmail.com")
+        var subject = "subjectname"
+        var body = "txtmessage"
+        var ei = Intent()
+        ei.setAction(Intent.ACTION_SEND)
+        ei.setType("message/rfc822")
+        ei.putExtra(Intent.EXTRA_EMAIL,emil)
+        ei.putExtra(Intent.EXTRA_SUBJECT,subject)
+        ei.putExtra(Intent.EXTRA_TEXT,body)
+        startActivity(ei)
+        Toast.makeText(this, "open the email ", Toast.LENGTH_SHORT).show()
+
     }
 }
